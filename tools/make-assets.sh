13ead60a40e3c4afb5b175ccd6c809f32dfffcf7
tinnamchoi <tinnam.choi@gmail.com>
2023-08-07 20:03:44 +0930

    chore: move source code into src
    tools/make-assets.sh modified accordingly

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-zip MESS_$1.chromium.zip manifest.json stylesheet.css
-zip MESS_$1.firefox.xpi manifest.json stylesheet.css
+zip MESS_$1.chromium.zip src/manifest.json src/stylesheet.css
+zip MESS_$1.firefox.xpi src/manifest.json src/stylesheet.css
